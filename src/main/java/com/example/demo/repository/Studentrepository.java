55fb078a502be19b32baa0e96f639b0fdf09df97
bala-amypo <bala@amypo.in>
2025-12-17 06:28:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/Studentrepository.java
+++ src/main/java/com/example/demo/repository/Studentrepository.java
@@ -3,4 +3,6 @@ import org.springframework.data.jpa.repository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.entity.Student;
 @Repository
-public interface StudentRepo extends
+public interface StudentRepository extends JpaRepository<Student,Long>{
+
+}
---
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.service.Impl;
+import java.util.*;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.Student;
+
+@Service
+public class StudentServiceImple
+implements StudentService{
+    private final Map<Long,Student>
+    store=new HashMap
+}
